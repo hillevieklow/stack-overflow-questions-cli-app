f1d15492a8a00dfe81d7eea5cf76b9d1f3b589dd
Hillevi Eklow <hillevieklow@gmail.com>
2018-06-23 16:12:47 -0400

    first commit

---
+++ lib/stack_overflow/cli.rb
@@ -0,0 +1,19 @@
+class StackOverflow::CLI
+  def call
+    list_questions
+    menu
+    goodbye
+  end
+
+  def list_questions
+    puts "La la la"
+  end
+
+  def menu
+    puts "Wohooo"
+  end
+
+  def goodbye
+    puts "See you later, aligator"
+  end
+end
