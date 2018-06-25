e7e4db5641fbd311b09fd959485fd4c4309b7552
Hillevi Eklow <hillevieklow@gmail.com>
2018-06-25 15:50:42 -0400

    added a method to scrape question details

---
+++ lib/stack_overflow/scraper.rb
@@ -0,0 +1,57 @@
+class Scraper
+
+  @@url_featured = "https://stackoverflow.com/questions?sort=featured"
+  @@url_newest = "https://stackoverflow.com/questions?sort=newest"
+
+  def self.setup_scrape
+    Question.create(self.scrape_newest)
+    Question.create(self.scrape_featured)
+    Question.all.each do |question|
+      details = self.scrape_question_details(question.link)
+      question.add_details(details)
+    end
+  end
+
+  def self.scrape_newest
+    doc = Nokogiri::HTML(open(@@url_newest))
+
+    questions = []
+    doc.css("div#questions.flush-left").each do |question|
+      question.css(".question-summary").each do |q|
+        question = q.css("h3").text
+        description_short = q.css(".excerpt").text.strip
+        stack_link = q.css("h3 a").attr("href")
+        link = "https://stackoverflow.com" + stack_link
+        type = "newest"
+        questions << {:question => question, :description_short => description_short, :link => link, :type => type}
+      end
+    end
+    questions
+  end
+
+  def self.scrape_featured
+    doc = Nokogiri::HTML(open(@@url_featured))
+    questions = []
+    doc.css("div#questions.flush-left").each do |question|
+      question.css(".question-summary").each do |q|
+        question = q.css("h3").text
+        description_short = q.css(".excerpt").text.strip
+        stack_link = q.css("h3 a").attr("href")
+        link = "https://stackoverflow.com" + stack_link
+        type = "featured"
+        questions << {:question => question, :description_short => description_short, :link => link, :type => type}
+      end
+    end
+    questions
+  end
+
+  def self.scrape_question_details(question_url)
+    doc = Nokogiri::HTML(open(question_url))
+    question_data = {}
+    question_data[:user] = doc.css("div.user-details a").text
+    question_data[:description_long] = doc.css("div.post-text").text.strip
+    question_data[:best_answer] = " "
+    question_data
+  end
+
+end
