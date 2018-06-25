0c30b1b05dba8ca9e4e63c18e9a152f8133990cb
Hillevi Eklow <hillevieklow@gmail.com>
2018-06-25 12:50:42 -0400

    improved question class

--- lib/stack_overflow/question.rb
+++ lib/stack_overflow/question.rb
@@ -1,23 +1,39 @@
 require 'pry'
-class StackOverflow::Question
-  attr_accessor :question, :description_short, :description_long, :votes, :answers, :tags
+class Question
+  attr_accessor :question, :link, :description_short, :description_long, :votes, :answers, :tags
 
-  def self.today
-    self.scrape_questions
+  @@all = []
+  @@newest =[]
+  @@featured = []
+
+  def initialize(questions_hash)
+    @question = questions_hash[:question]
+    @link = questions_hash[:link]
+    @description_short = questions_hash[:description_short]
+    @type = questions_hash[:type]
+
+    if @type.downcase = "newest"
+      @@newest << self
+    elsif @type.downcase = "featured"
+      @@featured << self
+    end
+    @@all << self
   end
 
-  def self.scrape_questions
-    questions = []
-    questions << self.scrape_stack_overflow
-    questions
+  def self.create(questions_array)
+    questions_array.each do |hash|
+      self.new(hash)
+    end
+
+  def self.all
+    @@all
   end
 
-  def self.scrape_stack_overflow
-    doc = Nokogiri::HTML(open("https://stackoverflow.com/questions"))
-    # binding.pry
-    question = self.new
-    question.question = doc.css("div#questions.flush-left h3").first.text.strip
-    question.description_short = doc.css("div#questions.flush-left .excerpt").first.text.strip
-    question
+  def self.newest
+    @@newest
+  end
+
+  def self.featured
+    @@featured
   end
 end
