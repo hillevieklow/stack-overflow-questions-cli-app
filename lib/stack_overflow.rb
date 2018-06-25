2d57a86cbfcc399fb2bb08f1cfe025664748f7db
Hillevi Eklow <hillevieklow@gmail.com>
2018-06-25 15:51:53 -0400

    cleaned up requirements list

--- lib/stack_overflow.rb
+++ lib/stack_overflow.rb
@@ -1,6 +1,7 @@
 require 'open-uri'
 require 'nokogiri'
 require 'pry'
+require 'colorize'
 require_relative "stack_overflow/version"
 require_relative './stack_overflow/question'
 require_relative './stack_overflow/cli'
